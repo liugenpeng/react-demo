1d767298b4d4fd11368b8db2365249a71e243005
liugenpeng <liugenpeng@corp.beisencorp.com>
2015-11-18 12:07:40 +0800

    add helloworld

---
+++ src/HelloWorld.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var HelloWorld = React.createClass({
+	render(){
+		
+		return (
+			<h1>
+				恭喜你运行成功
+			</h1>
+		);
+	}
+});
+
+module.exports = HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM  = require('react-dom');
+var HelloWorld  = require('./HelloWorld');
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
